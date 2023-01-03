5cc215f7adb17706a49c728dedee70215a715fcb
Joseph <jrenderle13@gmail.com>
2023-01-03 11:50:10 -0500

    fix problem with flipping directions"

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
   <div class="screen-container">
     <div class="data-container">
       <p>X: {{ Math.floor(circleProps[0].x) }}</p>
-      <p>X Velocity: {{ circleProps[0].velocityX * -1 }}</p>
-      <p>Y Velocity: {{ circleProps[0].velocityY * -1 }}</p>
+      <p>X Velocity: {{ circleProps[0].velocityX }}</p>
+      <p>Y Velocity: {{ circleProps[0].velocityY }}</p>
 
       <p>Accelerating: {{ accelerating }}</p>
       <p>Acceleration: {{ circleProps[0].acceleration }}</p>
@@ -15,6 +15,8 @@
       <p>Angular Acceleration: {{ circleProps[0].angularAcceleration }}</p>
 
       <p>Angular Velocity: {{ circleProps[0].angularVelocity * 1000 }}</p>
+      <p> Velocity: {{ circleProps[0].velocity }}</p>
+
 
       <button @click="flipXdirection">Flip x direction</button>
     </div>
@@ -26,14 +28,14 @@
 </template>
 <script setup lang="ts">
 import { timer } from '../src/services/timer';
-import { onMounted, reactive, Ref, ref, watch } from 'vue';
+import { computed, onMounted, reactive, Ref, ref, watch } from 'vue';
 import { degreesToRadians, radiansToDegrees, round } from './helpers/helpers';
 import MainCanvas, { Circles } from './components/Canvas.vue';
 let debounce = ref(false);
 let debounceAngle = ref(false);
 
-function flipXdirection(){
-  if (circleProps.value[0].angle === 0)circleProps.value[0].angle = 180;
+function flipXdirection() {
+  if (circleProps.value[0].angle === 0) circleProps.value[0].angle = 180;
   else circleProps.value[0].angle = 0;
 
 }
@@ -43,7 +45,14 @@ function accelerate(e: any) {
     if (!debounce.value) {
       debounce.value = true;
       setTimeout(() => {
-        if (accelerating.value) handleAccelerate(.001, 'x')
+
+        if (!isVelocityPositive.value) {
+          if (accelerating.value) handleAccelerate(.001, 'x')
+
+        } else {
+          if (accelerating.value) handleAccelerate(.001, 'x')
+
+        }
         debounce.value = false;
       }, 200)
     }
@@ -114,6 +123,18 @@ const circleProps: Ref<Circles[]> = ref([{
 let accelerating = ref(false);
 let spinning = ref(false);
 
+const isVelocityPositive = computed(() => {
+
+
+
+  if (circleProps.value[0].angle < 270 && circleProps.value[0].angle > 90) {
+    return true
+  } else {
+    return false
+  }
+
+}
+)
 
 
 let time = ref(0);
@@ -142,43 +163,18 @@ onMounted(() => {
   })
 })
 
-// watch(angularAccelerationRef, (newTime, oldTime) => {
-
-//   const dTime = 1;
-//   const { newAngularVelocity, newAngle } = startRotating(dTime, circleProps.value[0].angularVelocity, circleProps.value[0].angle, circleProps.value[0].angularAcceleration)
-
-//   circleProps.value[0].angularVelocity = newAngularVelocity;
-//   circleProps.value[0].angle = newAngle;
-//   time.value++;
-//   if (!accelerating.value && !spinning.value) {
-//     timer.start(() => {
-//       const { newAngularVelocity, newAngle } = startRotating(dTime, circleProps.value[0].angularVelocity, circleProps.value[0].angle, circleProps.value[0].angularAcceleration)
-
-//       circleProps.value[0].angularVelocity = newAngularVelocity;
-//       circleProps.value[0].angle = newAngle;
-
-//       time.value++
-//     })
-//   }
-
-//   })
-
 
 
 watch([accelerationRef, angularAccelerationRef], ([newAccleration, oldAcceleration], [newAngularAccleration, oldAngularAcceleration]) => {
 
   const dTime = 1;
-  let accelerationChanged = newAccleration !== oldAcceleration;
-  let angularAccelerationChanged = newAngularAccleration !== oldAngularAcceleration;
-
   const { position, newVelocity, newVelocityX, newVelocityY } = startMovingVector(dTime, circleProps.value[0].velocity, circleProps.value[0].x, circleProps.value[0].y, circleProps.value[0].angularVelocity, circleProps.value[0].angle, circleProps.value[0].acceleration);
-
-  const { newAngularVelocity, newAngle } = startRotating(dTime, circleProps.value[0].angularVelocity, circleProps.value[0].angle, circleProps.value[0].angularAcceleration)
-
+  const { newAngularVelocity, newAngle: calculatedAngle } = startRotating(dTime, circleProps.value[0].angularVelocity, circleProps.value[0].angle, circleProps.value[0].angularAcceleration)
   circleProps.value[0].x = position.x;
   circleProps.value[0].y = position.y;
   circleProps.value[0].velocity = accelerating.value ? newVelocity : circleProps.value[0].velocity
-  circleProps.value[0].angle = newAngle
+
+  circleProps.value[0].angle = calculatedAngle
   circleProps.value[0].angularVelocity = newAngularVelocity
   circleProps.value[0].velocityX = accelerating.value ? newVelocityX : circleProps.value[0].velocityX
   circleProps.value[0].velocityY = accelerating.value ? newVelocityY : circleProps.value[0].velocityY
@@ -186,15 +182,12 @@ watch([accelerationRef, angularAccelerationRef], ([newAccleration, oldAccelerati
 
   if (!accelerating.value && !spinning.value) {
     timer.start(() => {
-      const { position, newVelocity, newVelocityX, newVelocityY } = startMovingVector(dTime, circleProps.value[0].velocity, circleProps.value[0].x, circleProps.value[0].y, circleProps.value[0].angularVelocity, circleProps.value[0].angle, circleProps.value[0].acceleration);
-      const { newAngularVelocity, newAngle } = startRotating(dTime, circleProps.value[0].angularVelocity, circleProps.value[0].angle, circleProps.value[0].angularAcceleration)
+      const radians = degreesToRadians(calculatedAngle);
+      const position = calculatePositionVector(circleProps.value[0].x, circleProps.value[0].y, circleProps.value[0].velocity, dTime, circleProps.value[0].acceleration, radians)
+      const { newAngle } = startRotating(dTime, circleProps.value[0].angularVelocity, circleProps.value[0].angle, circleProps.value[0].angularAcceleration)
       circleProps.value[0].x = position.x;
       circleProps.value[0].y = position.y;
-      circleProps.value[0].velocity = accelerating.value ? newVelocity : circleProps.value[0].velocity
       circleProps.value[0].angle = newAngle
-      circleProps.value[0].angularVelocity = newAngularVelocity
-      circleProps.value[0].velocityX = accelerating.value ? newVelocityX : circleProps.value[0].velocityX
-      circleProps.value[0].velocityY = accelerating.value ? newVelocityY : circleProps.value[0].velocityY
       time.value++
     })
   }
@@ -202,39 +195,7 @@ watch([accelerationRef, angularAccelerationRef], ([newAccleration, oldAccelerati
 
 })
 
-// function startMoving(time: number, velocity: number, startingPosition: number, angularVelocity: number, originalAngle: number, acceleration: number) {
-//   // const force = calculateForce(10, 0);
-//   const position = calculatePosition(startingPosition, velocity, time, acceleration);
-//   const newVelocity = calculateVelocity(velocity, acceleration, time);
-//   const newAngle = calculateAngle(originalAngle, angularVelocity, time, angularAcceleration.value)
-//   const newAngularVelocity = calculateAngularVelocity(angularVelocity, angularAcceleration.value, time);
-//   return { position, newVelocity, newAngularVelocity, newAngle }
-// }
 
-// function calculateForce(mass: number, acceleration: number) {
-//   return mass * acceleration;
-// }
-
-
-// s_x = s_o_x + v_x * t + a_x * t^2 / 2
-// s_y = s_o_y + v_y * t + a_y * t^2 / 2
-// v_x = v_o_x + a_x * t
-// v_y = v_o_y + a_y * t
-
-// function calculatePositionXy(xStarting: any, xVelocity: any, xAcceleration: any, yStarting: any, yVelocity: any, yAcceleration: any, time: any) {
-//   let x;
-//   let y;
-
-//   function calculatePosition(originalPosition: number, velocity: number, time: number, acceleration: number) {
-//     return originalPosition + (velocity * time) + (acceleration * ((time ^ time) / 2));
-
-
-//   }
-//   return {
-//     x,
-//     y,
-//   }
-// }
 
 function startRotating(time: number, angularVelocity: number, originalAngle: number, angularAcceleration: number) {
   const newAngle = calculateAngle(originalAngle, angularVelocity, time, angularAcceleration)
@@ -256,9 +217,6 @@ function startMovingVector(time: number, velocity: number, positionX: number, po
 
 
 
-function calculatePosition(originalPosition: number, velocity: number, time: number, acceleration: number) {
-  return originalPosition + (velocity * time) + (acceleration * ((time ^ time) / 2));
-}
 
 function calculatePositionVector(originalX: number, originalY: number, velocity: number, time: number, acceleration: number, angle: number) {
   const x = originalX + (velocity * time * Math.cos(angle)) + ((1 / 2) * acceleration * (time * time));
@@ -267,14 +225,36 @@ function calculatePositionVector(originalX: number, originalY: number, velocity:
 }
 
 function calculateVelocityX(velocity: number, acceleration: number, time: number, angle: number) {
-  console.log(velocity);
-  return (velocity * Math.cos(angle)) + (acceleration * time);
+  const degrees = radiansToDegrees(angle);
+  if (degrees === 90 || degrees === 270) {
+    return 0;
+  }
+
+  const result = (velocity * Math.cos(angle)) + (acceleration * time);
+
+  if (velocity > 0) {
+    return Math.abs(result);
+  } else {
+    return -Math.abs(result)
+
+  }
+
+
 }
 function calculateVelocityY(velocity: number, acceleration: number, time: number, angle: number) {
   if (angle === 0 || angle == Math.PI) {
     return 0;
   }
-  return (velocity * Math.sin(angle)) + (acceleration * time);
+
+  const result = (velocity * Math.sin(angle)) + (acceleration * time);
+
+
+  if (velocity > 0) {
+    return Math.abs(result);
+  } else {
+    return -Math.abs(result)
+
+  }
 }
 
 function calculateAngle(originalAngle: number, angularVelocity: number, time: number, angularAcceleration: number) {
@@ -292,7 +272,14 @@ function calculateAngle(originalAngle: number, angularVelocity: number, time: nu
 }
 
 function calculateVelocity(originalVelocity: number, acceleration: number, time: number) {
-  return originalVelocity + (acceleration * time);
+  console.log(isVelocityPositive.value)
+  if (!isVelocityPositive.value) {
+    return originalVelocity + (acceleration * time);
+
+  } else {
+    return originalVelocity - (acceleration * time);
+
+  }
 }
 
 
